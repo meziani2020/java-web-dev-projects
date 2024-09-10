319c3ce323d1819605e887cb2fc83449f111cc29
Mohamed <issine14@gmail.com>
2024-09-09 21:07:40 -0500

    "exercices collections"

---
+++ exercises/collections-exercises/src/main/org/exeercicefiles/MainApp.java
@@ -0,0 +1,23 @@
+
+package main.org.exeercicefiles;
+
+import java.util.Arrays;
+
+public class MainApp {
+
+   public static void main(String[] args) {
+
+
+       System.out.println("no yes ");
+       int[] inntArray={2,1,2,3,4,5,8};
+      for (int nbr: inntArray){
+           System.out.println(nbr);
+      }
+      //---------------------
+       String txt="I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+      String[] subtxts=txt.split(" ");
+      System.out.println(Arrays.toString(subtxts));
+
+
+    }
+}
